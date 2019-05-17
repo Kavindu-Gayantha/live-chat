9ffa011b601d6e04e050d3a5f1410c982c704432
kavindu Gayantha <96kavindugayantha@gmail.com>
2019-05-17 23:38:47 +0530

    first changes

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head><title>lets chat</title>
+  <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+<body>
+  <div class = "container-fluid">
+    <div class ="well text-center" id ="chatBox">
+      <h3> Live Chat Room </h3>
+      <p>
+        <span>Kavindu : </span>
+        <span>The first chat applicatio</span>
+        <span>10.43 PM</span>
+      </p>
+    </div>
+
+
+
+</body>
+</html>
